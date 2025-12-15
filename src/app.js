b459ee88bc5ddccacb7fd78d4a0476c24b26134c
Tiagliveira <tiagliveira92@gmail.com>
2025-12-15 15:44:39 -0300

    feat: atulizando o cors da aplicacao

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ import 'dotenv/config';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/product-file', fileRoutesConfig);
